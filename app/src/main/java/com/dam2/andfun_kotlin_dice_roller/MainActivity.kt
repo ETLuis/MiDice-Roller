a455672ce13f6edb4bdd07dee43e2f7e6f0dc469
ETLuis <lcampanogoldaro@gmail.com>
2022-10-10 12:35:34 +0200

    Cambie las imagenes en el codigo.

--- app/src/main/java/com/dam2/andfun_kotlin_dice_roller/MainActivity.kt
+++ app/src/main/java/com/dam2/andfun_kotlin_dice_roller/MainActivity.kt
@@ -27,12 +27,12 @@ class MainActivity : AppCompatActivity() {
         val randomInt = Random().nextInt(6) + 1
 
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.ic_brujula
+            2 -> R.drawable.ic_octopus
+            3 -> R.drawable.ic_telefono
+            4 -> R.drawable.ic_pelota_de_tenis
+            5 -> R.drawable.ic_pelota_de_beisbol
+            else -> R.drawable.empty_dice
         }
 
         diceImage.setImageResource(drawableResource)
