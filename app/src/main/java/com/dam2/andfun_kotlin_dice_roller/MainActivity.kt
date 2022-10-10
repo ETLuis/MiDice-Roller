7db31d55591192308a5bb63934bfef20337fbb3d
ETLuis <lcampanogoldaro@gmail.com>
2022-10-10 10:06:44 +0200

    Step 5: Añadido la busqueda de views.

--- app/src/main/java/com/dam2/andfun_kotlin_dice_roller/MainActivity.kt
+++ app/src/main/java/com/dam2/andfun_kotlin_dice_roller/MainActivity.kt
@@ -9,14 +9,18 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -31,7 +35,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
