b1b8a4ed033bf6326ba5fea321261bef4ff94a5d
ETLuis <lcampanogoldaro@gmail.com>
2022-10-10 09:52:43 +0200

    Step 2: Añadido el onClickListener.

---
+++ app/src/main/java/com/dam2/andfun_kotlin_dice_roller/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dam2.andfun_kotlin_dice_roller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
